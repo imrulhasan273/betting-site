349d10fccb5df02b1db137a3d233aaff42c95c21
Md. Imrul Hasan <imrulhasan273@gmail.com>
2020-08-11 23:25:57 +0600

    user profile:fefault

--- resources/views/frontend_partials/userpanel.blade.php
+++ resources/views/frontend_partials/userpanel.blade.php
@@ -1,6 +1,7 @@
 @if (Auth::user())
 <div id="mySidenav" class="sidenav">
-    <h2 style="padding-top: 18%;"><i style="font-size: 27px;
+    <h2 style="padding-top: 18%;">
+        <i style="font-size: 27px;
                     left: 7px;color:#fff;
                     position: absolute;
                     top: 18px;" class="fa fa-bars fa-lg " onclick="closeNav();" aria-hidden="true">
@@ -14,11 +15,8 @@
     </h2>
     <div class="eos-menu" id="menu">
         <div class="eos-menu-content" style="height:360px;">
-        <div class="eos-group-title"><i class="fa fa-user-tie" aria-hidden="true"></i> </div>
         @if (Auth::user())
-            <a style="padding:0px;font-size: 14px;" href="oneten.php">
-                <div class="eos-group-title"><i aria-hidden="true"><img src="frontend/img/user1.png" style="max-width: 22px;"></i> {{ Auth::user()->name }}</div>
-            </a>
+        <div class="eos-group-title"><i class="fa fa-user-tie" aria-hidden="true"></i> {{ Auth::user()->name }}</div>
         @endif
         <a style="padding:0px;font-size: 14px;" href="oneten.php">
             <div class="eos-group-title"><i aria-hidden="true"><img src="frontend/img/oneten.gif" style="max-width: 22px;"></i> OneTen</div>
