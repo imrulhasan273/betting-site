f34f52abdefa2c597ba61a506b963b94c5241698
Md. Imrul Hasan <imrulhasan273@gmail.com>
2020-08-11 19:58:41 +0600

    All the Permission Type added

--- README.md
+++ README.md
@@ -6,5 +6,6 @@
 
 ```cmd
 ~$ php artisan make:migration create_role_user_table
+$ php artisan make:middleware RoleChecker
 
 ```
---
+++ app/Http/Middleware/RoleChecker.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class RoleChecker
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $super_adminRole, $adminRole,  $clubRole)
+    {
+        $roles = Auth::check() ? Auth::user()->role->pluck('name')->toArray() : [];
+
+        if (in_array($super_adminRole, $roles)) {
+            return $next($request);
+        } else if (in_array($adminRole, $roles)) {
+            return $next($request);
+        } else if (in_array($clubRole, $roles)) {
+            return $next($request);
+        }
+
+        return Redirect::route('home');
+    }
+}
