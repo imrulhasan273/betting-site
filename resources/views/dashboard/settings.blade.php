f40cc2bb30f030697ff78966eac45b54988d4095
Md. Imrul Hasan <imrulhasan273@gmail.com>
2020-08-12 09:30:33 +0600

    setting:backend

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -4,77 +4,66 @@
 @extends('layouts.backend')
 @section('content')
 <div class="row">
-    <div class="col-sm-6 cat-form">
-        {{-- <h1> if working, row count = {{ $settings->count() }}</h1> --}}
-        <form action="" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="form-group">
-                {{-- <label>Logo</label> --}}
-                <p><input type="file" accept="image/*" name="image" id="file" onchange="loadFile(event)"
+{{-- <h1> if working, row count = {{ $settings->count() }}</h1> --}}
+    <div class="card col-sm-6 cat-form">
+        <div class="card-header card-header-primary">
+            <h4 class="card-title ">Setup your site</h4>
+            <p class="card-category"> Logo and footer text</p>
+        </div>
 
-           <div class="row">
-
-        <div class="col-md-12">
-
-                {{-- <h1> if working, row count = {{ $settings->count() }}</h1> --}}
-              <div class="card">
-                   <div class="card-header card-header-primary">
-                  <h4 class="card-title ">Setup your site</h4>
-                  <p class="card-category"> Logo and footer text</p>
+        <div class="card-body">
+            <form action="" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    {{-- <label>Logo</label> --}}
+                    <p><input type="file" accept="image/*" name="image" id="file" onchange="loadFile(event)"
+                                            style="display: none;"></p>
+                    <p><label for="file" class="btn btn-warning" style="cursor: pointer;">Upload Image</label></p>
+                    <p><img id="output" width="200" /></p>
+                    <p>Website Logo.</p>
                 </div>
 
-                <div class="card-body">
-            <form action="" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label>Footer text</label>
+                    <textarea name="footer" id="footer" class="form-control" rows="5">
+                    </textarea>
+                </div>
+
+                <div class="form-group">
+                    <button class="btn btn-primary">Add Settings</button>
+                </div>
+            </form>
+
+            {{-- <h1>Else working, row count = {{$settings->count() }}</h1> --}}
+
+                {{-- <form action="" method="POST" enctype="multipart/form-data">
                     @csrf
-            <div class="form-group">
-                        {{-- <label>Logo</label> --}}
+                    <div class="form-group">
+                        <label>Logo</label>
                         <p><input type="file" accept="image/*" name="image" id="file" onchange="loadFile(event)"
-                                style="display: none;"></p>
-                <p><label for="file" class="btn btn-warning" style="cursor: pointer;">Upload Image</label></p>
-                <p><img id="output" width="200" /></p>
-                <p>Website Logo.</p>
-            </div>
+                                            style="display: none;"></p>
+                        <p><label for="file" class="btn btn-warning" style="cursor: pointer;">Upload Image</label>
+                        </p>
+                        <p><img id="output" width="200" /></p>
 
-            <div class="form-group">
-                <label>Footer text</label>
-                <textarea name="footer" id="footer" class="form-control" rows="5">
-                </textarea>
-            </div>
+                        <img src="" width="100">
 
-            <div class="form-group">
-                <button class="btn btn-primary">Add Settings</button>
-            </div>
-        </form>
-                {{-- <h1>Else working, row count = {{$settings->count() }}</h1> --}}
-
-                    {{-- <form action="" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <div class="form-group">
-                            <label>Logo</label>
-                            <p><input type="file" accept="image/*" name="image" id="file" onchange="loadFile(event)"
-                                    style="display: none;"></p>
-                            <p><label for="file" class="btn btn-warning" style="cursor: pointer;">Upload Image</label>
-                            </p>
-                            <p><img id="output" width="200" /></p>
-
-                            <img src="" width="100">
-
-                            <p>Website Logo.</p>
-                        </div>
+                        <p>Website Logo.</p>
+                    </div>
 
 
-                        <div class="text-right">
-                            <span class="btn btn-warning" id="addSocialField"><i class="fa fa-plus"></i></span>
-                        </div>
-                        <div class="form-group">
-                            <label>About US</label>
-                            <textarea name="about" id="about" class="form-control"
-                                rows="5">  </textarea>
-                        </div>
-                        <div class="form-group">
-                            <button class="btn btn-primary">Update Settings</button>
-                        </div>
-                    </form> --}}
+                    <div class="text-right">
+                        <span class="btn btn-warning" id="addSocialField"><i class="fa fa-plus"></i></span>
+                    </div>
+                    <div class="form-group">
+                        <label>About US</label>
+                        <textarea name="about" id="about" class="form-control"
+                                    rows="5">  </textarea>
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-primary">Update Settings</button>
+                    </div>
+                </form> --}}
 
         </div>
     </div>
