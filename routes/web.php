c926053f16d2c13ae61ba02837d428e6ef87dc5b
Md. Imrul Hasan <imrulhasan273@gmail.com>
2020-08-12 17:38:52 +0600

    upd

--- routes/web.php
+++ routes/web.php
@@ -24,15 +24,18 @@ Route::get('/rules', 'HomeController@rules')->name('rules');
 
 
 # PROFILE
-Route::get('/wallet/profile', 'ProfileController@index')->name('profiles.profile');
-Route::get('/wallet/deposit', 'ProfileController@index')->name('profiles.deposit');
-Route::get('/wallet/widthdraw', 'ProfileController@index')->name('profiles.widthdraw');
-Route::get('/wallet/b-transfer', 'ProfileController@index')->name('profiles.btransfer');
-Route::get('/wallet/change-club', 'ProfileController@index')->name('profiles.changeclub');
-Route::get('/wallet/change-password', 'ProfileController@index')->name('profiles.changepassword');
-Route::get('/wallet/statement', 'ProfileController@index')->name('profiles.statement');
-Route::get('/wallet/sponsor', 'ProfileController@index')->name('profiles.sponsor');
-Route::get('/wallet/oneten', 'ProfileController@index')->name('profiles.oneten');
+route::group(['prefix' => 'wallet'], function () {
+    Route::get('/wallet/profile', 'ProfileController@index')->name('profiles.profile');
+    Route::get('/wallet/deposit', 'ProfileController@index')->name('profiles.deposit');
+    Route::get('/wallet/widthdraw', 'ProfileController@index')->name('profiles.widthdraw');
+    Route::get('/wallet/b-transfer', 'ProfileController@index')->name('profiles.btransfer');
+    Route::get('/wallet/change-club', 'ProfileController@index')->name('profiles.changeclub');
+    Route::get('/wallet/change-password', 'ProfileController@index')->name('profiles.changepassword');
+    Route::get('/wallet/statement', 'ProfileController@index')->name('profiles.statement');
+    Route::get('/wallet/sponsor', 'ProfileController@index')->name('profiles.sponsor');
+    Route::get('/wallet/oneten', 'ProfileController@index')->name('profiles.oneten');
+});
+
 # PROFILE
 
 
