ca9e98dc245a2547cd859896dd11ba206447ab4d
Md. Imrul Hasan <imrulhasan273@gmail.com>
2020-08-13 14:20:46 +0600

    Validation Error Message + Success message

--- resources/views/dashboard/users/add.blade.php
+++ resources/views/dashboard/users/add.blade.php
@@ -5,6 +5,19 @@ $active='users';
 
 @section('content')
 <div class="row">
+
+    <div class="col-md-12">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+    </div>
+
     <div class="col-md-12">
       <div class="card">
         <div class="card-header card-header-primary">
