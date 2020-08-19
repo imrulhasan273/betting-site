aeeab28b41877b83838e5e63bf2f43043a993210
Md. Imrul Hasan <imrulhasan273@gmail.com>
2020-08-19 14:05:03 +0600

    button addded

--- resources/views/dashboard/games.blade.php
+++ resources/views/dashboard/games.blade.php
@@ -24,59 +24,59 @@ $active='games';
           <div class="table-responsive">
             <table class="table table-hover">
               <thead class="">
-                <th>
+                <th class="td-actions text-center">
                     ID
                 </th>
-                <th>
+                <th class="td-actions text-center">
                     Type
                 </th>
-                <th>
+                <th class="td-actions text-center">
                     Name
                 </th>
-                <th>
+                <th class="td-actions text-center">
                     Tournament
                 </th>
-                <th>
+                <th class="td-actions text-center">
                     Day
                 </th>
-                <th>
+                <th class="td-actions text-center">
                     Time
                 </th>
-                <th>
+                <th class="td-actions text-center">
                     Update
                 </th>
-                <th>
+                <th class="td-actions text-center">
                     Betting Option
                 </th>
-                <th>
+                <th class="td-actions text-center">
                     Status
                 </th>
-                <th>
+                <th class="td-actions text-center">
                     Action
                 </th>
               </thead>
               @foreach ($games as $game)
                 <tbody>
                     <tr>
-                        <td>
+                        <td class="td-actions text-center">
                             {{$game->id}}
                         </td>
-                        <td>
+                        <td class="td-actions text-center">
                             {{$game->type->name}}
                         </td>
-                        <td>
+                        <td class="td-actions text-center">
                             {{$game->name}}
                         </td>
-                        <td>
+                        <td class="td-actions text-center">
                             {{$game->tournament_name}}
                         </td>
-                        <td>
+                        <td class="td-actions text-center">
                             {{$game->date}}
                         </td>
-                        <td>
+                        <td class="td-actions text-center">
                             {{$game->time}}
                         </td>
-                        <td class="td-actions text-left">
+                        <td class="td-actions text-center">
                             {{$game->game_update}}
                             <a href="" type="button" rel="tooltip" class="btn btn-info">
                                 <i class="material-icons">sync</i>
@@ -87,13 +87,13 @@ $active='games';
                                 <i class="material-icons">gavel</i>
                             </a>
                         </td>
-                        <td class="td-actions text-left">
+                        <td class="td-actions text-center">
                             {{$game->status}}
                             <a href="{{ route('admin.games.status',[$game->id,1]) }}" type="button" rel="tooltip" class="btn btn-info">
                                 <i class="material-icons">av_timer</i>
                             </a>
                         </td>
-                        <td class="td-actions text-right">
+                        <td class="td-actions text-center">
                             <a href="{{ route('admin.games.status',[$game->id,2]) }}" type="button" rel="tooltip" class="btn btn-info">
                                 @if($game->status!='hidden')
                                 <i class="material-icons">check_circle</i>
@@ -108,7 +108,7 @@ $active='games';
                               <i class="material-icons">close</i>
                             </a>
                             <a href="" type="button" rel="tooltip" class="btn btn-info">
-                                <i class="material-icons">done_all</i>
+                                <i class="material-icons">done_all</i>done
                             </a>
                         </td>
                     </tr>
