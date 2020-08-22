545ced149e3fc21b9a380795856ee969fe35cb9a
Md. Imrul Hasan <imrulhasan273@gmail.com>
2020-08-22 23:03:22 +0600

    Bet Problem Arises

--- app/Http/Controllers/BetController.php
+++ app/Http/Controllers/BetController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Bet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BetController extends Controller
 {
@@ -11,8 +12,34 @@ class BetController extends Controller
     # ____________________________________Using Ajax__________________________________
     public function placeBit(Request $request)
     {
-        $data = $request->BETamount;
+        $gameID = $request->gameID;
+        $quesID = $request->quesID;
+        $ansID =  $request->ansID;
+        $BETreturnRate =  $request->BETreturnRate;
+        $BETamount =  $request->BETamount;
+        $match =  $request->match;
 
+        #
+        $userId = Auth::user()->id;
+        $total_win = $BETamount * $BETreturnRate;
+        $BetInsert = Bet::create([
+            'bet_by' =>  $userId,
+            'match' => $match,
+            'game_id' => $gameID,
+            'question_id' => $quesID,
+            'answer_id' => $ansID,
+            'amount' => $BETamount,
+            'return_rate' => $BETreturnRate,
+            'total_win' => $total_win,
+            'return_amount' => $total_win,
+            'club_fee' => 0.01,
+        ]);
+
+        if ($BetInsert) {
+            $data = 'Inserted';
+        } else {
+            $data = 'Not Inserted';
+        }
 
         return response()->json($data);
     }
--- resources/views/home/partials_home/bet_script.blade.php
+++ resources/views/home/partials_home/bet_script.blade.php
@@ -15,34 +15,28 @@ $('#betting').on('show.bs.modal', function (event) {
     let ques_id = button.data('ques_id');
     let ans_id = button.data('ans_id');
 
-
     // ============= Get the html value from Modal ================
     returnamount('100')
 
     $('#200').click(function(event) {
       $('#stakeAmount').val('200');
       returnamount('200')
-
     });
     $('#500').click(function(event) {
       $('#stakeAmount').val('500');
       returnamount('500')
-
     });
     $('#1000').click(function(event) {
       $('#stakeAmount').val('1000');
       returnamount('1000')
-
     });
     $('#3000').click(function(event) {
       $('#stakeAmount').val('3000');
       returnamount('3000')
-
     });
     $('#5000').click(function(event) {
       $('#stakeAmount').val('5000');
       returnamount('5000')
-
     });
 
     function returnamount(stakeamount) {
@@ -66,16 +60,11 @@ $('#betting').on('show.bs.modal', function (event) {
     modal.find('#quesID').val(ques_id);
     modal.find('#ansID').val(ans_id);
 
-
-
     if (status == 'live') {
         $(".gameLiveOrUpcoming").css("background", "#ec5d18");
     } else {
         $(".gameLiveOrUpcoming").css("background", "#6918ec");
     }
-
-    // console.log('Type: ');
-    // console.log(type_id);
 
     if (type_id == "1") {
         $('#gameLogo').attr('src', "{{asset('frontend/img/ka-pl.png')}}");
@@ -105,13 +94,16 @@ $("#placeBet").on("click", function() {
         var ansID = $("#ansID").val();
         var BETreturnRate = $("#betRateShow").text();
         var BETamount = $("#stakeAmount").val();
+        var match = $("#bettingTitle").text();
 
 
-        console.log(gameID);
-        console.log(quesID);
-        console.log(ansID);
-        console.log(BETreturnRate);
-        console.log(BETamount);
+        // console.log(gameID);
+        // console.log(quesID);
+        // console.log(ansID);
+        // console.log(BETreturnRate);
+        // console.log(BETamount);
+        // console.log(match);
+
 
         $("#placeBet").addClass("hideplace");
         // $("#load").removeClass("load");
@@ -125,6 +117,7 @@ $("#placeBet").on("click", function() {
                 ansID: ansID,
                 BETreturnRate: BETreturnRate,
                 BETamount: BETamount,
+                match : match,
             },
 
             success: function(data) {
@@ -134,341 +127,4 @@ $("#placeBet").on("click", function() {
         });
     }
 });
-
 </script>
-
-
-{{--  --}}
-
-
-{{-- <script type="text/javascript">
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-36251023-1']);
-        _gaq.push(['_setDomainName', '.net']);
-        _gaq.push(['_trajqueryscriptckPageview']);
-
-        (function() {
-          var ga = document.createElement('script');
-          ga.type = 'text/javascript';
-          ga.async = true;
-          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-          var s = document.getElementsByTagName('script')[0];
-          s.parentNode.insertBefore(ga, s);
-        })();
-
-        $('#tapbalance').click(function(event) {
-          $('#tbalance2').hide();
-          $('#tbalance').show();
-          setTimeout(function() {
-
-            $("#tbalance").hide();
-
-            $("#tbalance2").show();
-          }, 2000);
-        });
-
-        $('#show_deposit_notice').click(function(event) {
-          $('#deposit_notice').fadeIn('slow/900/fast', function() {
-
-          });
-
-          setTimeout(function() {
-            $("#deposit_notice").fadeOut('slow/400/fast', function() {
-
-            });
-          }, 10000);
-
-
-        });
-        $('#show_withdraw_notice').click(function(event) {
-          $('#withdraw_notice').fadeIn('slow/900/fast', function() {
-
-          });
-
-          setTimeout(function() {
-
-
-            $("#withdraw_notice").fadeOut('slow/400/fast', function() {
-
-            });
-          }, 10000);
-
-        });
-        //one ten game
-        $('#stakeAmountoneten').keyup(function() {
-          var stake = $('#stakeAmountoneten').val();
-          $('#onetentotalstake').html(stake);
-          var first = stake * 3;
-          var second = stake * 2.5;
-          var third = stake * 2;
-          $('#possibleAmount1').html(first);
-          $('#possibleAmount2').html(second);
-          $('#possibleAmount3').html(third);
-        });
-        var games_id = "";
-        var options_id = "";
-
-        function option_select(game_id, option_id, option_no) {
-          games_id = game_id;
-          options_id = option_id;
-          $('#select_option').html(option_no);
-        }
-        $(document).ready(function() {
-          $('#oneten_placebet').click(function(event) {
-            var stake = $('#stakeAmountoneten').val();
-            if (stake == "") {
-              alert('Give stake amount');
-            } else {
-              if (options_id == "") {
-                alert('select an option')
-              } else {
-                $.ajax({
-                  method: "POST",
-                //   url: 'oneten_betplace',
-
-                  data: {
-                    game_id: games_id,
-                    option_id: options_id,
-                    stake: stake
-                  },
-                  success: function(data) {
-
-                    if (data == "Bet Placed Successfuly") {
-
-                      $('#errorBetoneten').show();
-                      $('#errorBetoneten').removeClass('alert-success');
-                      $('#errorBetoneten').removeClass('alert-danger');
-                      $('#errorBetoneten').addClass('alert-success');
-                      $('#alertBetoneten').html(data);
-
-                      setTimeout(function() {
-                        $("#errorBetoneten").hide();
-                        $(".betForm").hide();
-                        location.reload();
-                      }, 1000);
-                    } else {
-
-                      $('#errorBetoneten').show();
-                      $('#errorBetoneten').removeClass('alert-success');
-                      $('#errorBetoneten').removeClass('alert-danger');
-                      $('#errorBetoneten').addClass('alert-danger');
-                      $('#alertBetoneten').html(data);
-                      betclick = 0;
-                      setTimeout(function() {
-
-                        $("#errorBetoneten").hide();
-                      }, 1000);
-
-                    }
-
-                  }
-                });
-              }
-            }
-
-          });
-
-        });
-        // oneten_refresh
-        // setInterval(function() {
-        //   $.ajax({
-        //     method: "POST",
-        //     url: 'oneten_refresh',
-        //     data: {
-        //       wAmount: '1'
-        //     },
-        //     success: function(data) {
-        //       // console.log(data);
-
-        //       if (data === "1") {
-        //         location.reload();
-
-        //       }
-        //     }
-        //   });
-        // }, 8000);
-
-        $('#200a').click(function(event) {
-          $('#stakeAmountoneten').val('200');
-          var stake = $('#stakeAmountoneten').val();
-          $('#onetentotalstake').html(stake);
-          var first = stake * 3;
-          var second = stake * 2.5;
-          var third = stake * 2;
-          $('#possibleAmount1').html(first);
-          $('#possibleAmount2').html(second);
-          $('#possibleAmount3').html(third);
-
-        });
-        $('#500a').click(function(event) {
-          $('#stakeAmountoneten').val('500');
-          var stake = $('#stakeAmountoneten').val();
-          $('#onetentotalstake').html(stake);
-          var first = stake * 3;
-          var second = stake * 2.5;
-          var third = stake * 2;
-          $('#possibleAmount1').html(first);
-          $('#possibleAmount2').html(second);
-          $('#possibleAmount3').html(third);
-
-        });
-        $('#1000a').click(function(event) {
-          $('#stakeAmountoneten').val('1000');
-          var stake = $('#stakeAmountoneten').val();
-          $('#onetentotalstake').html(stake);
-          var first = stake * 3;
-          var second = stake * 2.5;
-          var third = stake * 2;
-          $('#possibleAmount1').html(first);
-          $('#possibleAmount2').html(second);
-          $('#possibleAmount3').html(third);
-
-        });
-        $('#3000a').click(function(event) {
-          $('#stakeAmountoneten').val('3000');
-          var stake = $('#stakeAmountoneten').val();
-          $('#onetentotalstake').html(stake);
-          var first = stake * 3;
-          var second = stake * 2.5;
-          var third = stake * 2;
-          $('#possibleAmount1').html(first);
-          $('#possibleAmount2').html(second);
-          $('#possibleAmount3').html(third);
-
-        });
-        $('#5000a').click(function(event) {
-          $('#stakeAmountoneten').val('5000');
-          var stake = $('#stakeAmountoneten').val();
-          $('#onetentotalstake').html(stake);
-          var first = stake * 3;
-          var second = stake * 2.5;
-          var third = stake * 2;
-          $('#possibleAmount1').html(first);
-          $('#possibleAmount2').html(second);
-          $('#possibleAmount3').html(third);
-
-        });
-
-        $('#add_multibet').click(function(event) {
-          $.ajax({
-            method: "POST",
-            url: 'betfacedata_2',
-            //dataType: 'JSON',
-            data: {
-              bettingTitle: bettingTitle,
-              bettingSubTitle: bettingSubTitle,
-              BettingSubTitleOption: BettingSubTitleOption
-            },
-            success: function(data) {
-
-              $.ajax({
-                method: "GET",
-                // url: 'get_bet_slip',
-                data: {
-                  uid: 'ss'
-                },
-                success: function(datas) {
-
-                  if (data == '1') {
-                    $("#bet_slip").load('bet_slip');
-                    $('#bet_slip_div').css('display', 'block');
-
-                    show = 0;
-
-                  } else {
-                    alert(data);
-                    $("#bet_slip").load('bet_slip');
-                  }
-                }
-              });
-            }
-          });
-
-          count_multi();
-
-        });
-
-        function delete_bet_slip(id) {
-          $.ajax({
-            method: "POST",
-            // url: 'delete_bet_slip',
-            data: {
-              id: id
-            },
-            success: function(data) {
-              if (data == '1') {
-
-                $("#bet_slip").load('bet_slip');
-                count_multi();
-              }
-            }
-          });
-
-        }
-
-
-
-        // ============================BET SECTION ===========================
-        function seect_option(a, b, c)
-        {
-          if (multi == 0)
-          {
-            $('#betting').modal('show');
-          }
-          else
-          {
-            $.ajax({
-              method: "POST",
-              url: 'betfacedata_2',
-              //dataType: 'JSON',
-              data: {
-                bettingTitle: a,
-                bettingSubTitle: b,
-                BettingSubTitleOption: c
-              },
-              success: function(data) {
-
-                $.ajax({
-                  method: "GET",
-                //   url: 'get_bet_slip',
-                  data: {
-                    uid: 'ss'
-                  },
-                  success: function(datas) {
-
-                    if (data == '1')
-                    {
-                      $("#bet_slip").load('bet_slip');
-                      $('#bet_slip_div').css('display', 'block');
-                      show = 0;
-                      count_multi();
-
-                    }
-                    else
-                    {
-                      alert(data);
-                      $("#bet_slip").load('bet_slip');
-                    }
-                  }
-                });
-              }
-            });
-
-          }
-        }
-
-        function count_multi() {
-          $.ajax({
-            method: "GET",
-            // url: 'count_bet_slip',
-            data: {
-              id: "id"
-            },
-            success: function(data) {
-              $('#betslip_toolkit').show();
-              $('#total_multi').html(data);
-
-            }
-          });
-        }
-</script> --}}
