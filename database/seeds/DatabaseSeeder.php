e212ba42b766dc2d8356a242422eda399e3f6653
Md. Imrul Hasan <imrulhasan273@gmail.com>
2020-09-27 21:13:43 +0600

    Seeder disabled from Database Seeder for Now

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,20 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PaymentOptionSeeder::class);
-
-        $this->call(ClubSeeder::class);
+        // $this->call(PaymentOptionSeeder::class);
+        // $this->call(ClubSeeder::class);
 
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
 
         $this->call(TypeSeeder::class);
-        $this->call(TypesTableSeeder::class);
-        $this->call(GamesTableSeeder::class);
-        $this->call(QuestionsTableSeeder::class);
-        $this->call(AnswersTableSeeder::class);
+        // $this->call(TypesTableSeeder::class);
+        // $this->call(GamesTableSeeder::class);
+        // $this->call(QuestionsTableSeeder::class);
+        // $this->call(AnswersTableSeeder::class);
 
-        // $this->call(DepositSeeder::class);
+        #$this->call(DepositSeeder::class); //its not necessary.
 
         $this->call(CommissionSeeder::class);
     }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -27,76 +27,66 @@ class UserSeeder extends Seeder
         ]);
         $user->role()->attach($role->id);
 
-        #Admin Seeder
-        $role = Role::where('name', 'admin')->first();
-        $user = User::create([
-            'name' => 'Mr. Admin',
-            'user_name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('0000000000admin'),
-            'remember_token' => Str::random(60),
-        ]);
-        $user->role()->attach($role->id);
+        // #Admin Seeder
+        // $role = Role::where('name', 'admin')->first();
+        // $user = User::create([
+        //     'name' => 'Mr. Admin',
+        //     'user_name' => 'Admin',
+        //     'email' => 'admin@gmail.com',
+        //     'password' => Hash::make('0000000000admin'),
+        //     'remember_token' => Str::random(60),
+        // ]);
+        // $user->role()->attach($role->id);
 
-        #Club Admin Seeder
-        $club = Club::where('id', 1)->first();  //added
-        $role = Role::where('name', 'club_admin')->first();
-        $user = User::create([
-            'name' => 'Mr. Club Owner 1',
-            'user_name' => 'club1',
-            'email' => 'club1@gmail.com',
-            'password' => Hash::make('0000000000club1'),
-            'remember_token' => Str::random(60),
-        ]);
-        $user->role()->attach($role->id);
-        $user->clubOwner()->attach($club->id); //added
+        // #Club Admin Seeder
+        // $club = Club::where('id', 1)->first();  //added
+        // $role = Role::where('name', 'club_admin')->first();
+        // $user = User::create([
+        //     'name' => 'Mr. Club Owner 1',
+        //     'user_name' => 'club1',
+        //     'email' => 'club1@gmail.com',
+        //     'password' => Hash::make('0000000000club1'),
+        //     'remember_token' => Str::random(60),
+        // ]);
+        // $user->role()->attach($role->id);
+        // $user->clubOwner()->attach($club->id); //added
 
-        #Club Admin Seeder
-        $club = Club::where('id', 2)->first();   //added
-        $role = Role::where('name', 'club_admin')->first();
-        $user = User::create([
-            'name' => 'Mr. Club Owner 2',
-            'user_name' => 'club2',
-            'email' => 'club2@gmail.com',
-            'password' => Hash::make('0000000000club2'),
-            'remember_token' => Str::random(60),
-        ]);
-        $user->role()->attach($role->id);
-        $user->clubOwner()->attach($club->id);    //added
+        // #Club Admin Seeder
+        // $club = Club::where('id', 2)->first();   //added
+        // $role = Role::where('name', 'club_admin')->first();
+        // $user = User::create([
+        //     'name' => 'Mr. Club Owner 2',
+        //     'user_name' => 'club2',
+        //     'email' => 'club2@gmail.com',
+        //     'password' => Hash::make('0000000000club2'),
+        //     'remember_token' => Str::random(60),
+        // ]);
+        // $user->role()->attach($role->id);
+        // $user->clubOwner()->attach($club->id);    //added
 
 
-        ## Added a club id to the users
-        #Normal User Seeder
-        $role = Role::where('name', 'user')->first();
+        // ## Added a club id to the users
+        // #Normal User Seeder
+        // $role = Role::where('name', 'user')->first();
 
-        $user = User::create([
-            'name' => 'Mr. Normal User',
-            'user_name' => 'user',
-            'club_id' => 1,
-            'email' => 'user@gmail.com',
-            'password' => Hash::make('0000000000user'),
-            'remember_token' => Str::random(60),
-        ]);
-        $user->role()->attach($role->id);
+        // $user = User::create([
+        //     'name' => 'Mr. Normal User',
+        //     'user_name' => 'user',
+        //     'club_id' => 1,
+        //     'email' => 'user@gmail.com',
+        //     'password' => Hash::make('0000000000user'),
+        //     'remember_token' => Str::random(60),
+        // ]);
+        // $user->role()->attach($role->id);
 
-        # Normal User Club Registration
-        $count = Club::where('id', 1)->pluck('member');
-        $count = $count[0];
-        $updatingClub = Club::where('id', 1)->first();
-        if ($updatingClub) {
-            $updatingClub->update([
-                'member' => $count + 1,
-            ]);
-        }
-        // -- - - - - -- - -- - - - - ---
-        # Normal User Sponsor
-        // $superAdmin = User::whereHas(
-        //     'role',
-        //     function ($q) {
-        //         $q->where('name', 'super_admin');
-        //     }
-        // )->get();
-        // $superAdmin = $superAdmin[0];
-        // $user->ref()->attach($superAdmin->id);
+        // # Normal User Club Registration
+        // $count = Club::where('id', 1)->pluck('member');
+        // $count = $count[0];
+        // $updatingClub = Club::where('id', 1)->first();
+        // if ($updatingClub) {
+        //     $updatingClub->update([
+        //         'member' => $count + 1,
+        //     ]);
+        // }
     }
 }
